e7f6b4ebc46abf671a57998544f162914602615e
2425860793@qq.com <1020269294@qq.com>
2018-03-06 09:28:38 +0800

    feat: 添加csrf安全

--- config/config.default.js
+++ config/config.default.js
@@ -14,5 +14,6 @@ exports.security = {
 	csrf: {
 		queryName: '_csrf', // 通过 query 传递 CSRF token 的默认字段为 _csrf
 		bodyName: '_csrf', // 通过 body 传递 CSRF token 的默认字段为 _csrf
+		headerName: '_csrf'
 	},
 };
